5188340e1a6461a5ed273951d346a4c9a6a9c762
Ana Nikolova <ani.nik19@gmail.com>
2021-10-20 08:06:32 +0200

    close the pop up only once

--- senerio1/js/app.js
+++ senerio1/js/app.js
@@ -1,16 +1,29 @@
 window.onload = function(){
+    let emailState= false;
+
     let emailModal = document.getElementsByClassName("email-modal")[0]
     let closeModal = document.getElementsByClassName("email-modal__close-btn")[0]
    
+    let showModal=()=>{
+    if(emailState == false){
+    emailModal.classList.add("email-modal--visible")
+    emailState= true;
+    }
+        
+    }
+
     closeModal.addEventListener("click", ()=>{
      emailModal.classList.remove("email-modal--visible")
     })
 
 
-    document.addEventListener("mouseleave",()=>{
-    emailModal.classList.add("email-modal--visible")
+document.body.addEventListener("mouseleave",()=>{
+    showModal();
+    
+    });
 
-    })
+
+
     console.log(document)
-
 }
+
